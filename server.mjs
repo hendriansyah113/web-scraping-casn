cd5162c8f80317ec385a7c5e1a510471233f86af
hendriansyah113 <81542284+hendriansyah113@users.noreply.github.com>
2025-01-12 11:07:29 +0700

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,8 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const XLSX = require('xlsx');
-const cors = require('cors');
-const fs = require('fs');
+import express from 'express';
+import bodyParser from 'body-parser';
+import fetch from 'node-fetch';
+import XLSX from 'xlsx';
+import cors from 'cors';
+import fs from 'fs';
 
 const app = express();
 app.use(bodyParser.json());
@@ -22,9 +23,6 @@ const headers = {
 async function fetchAllData(params) {
     let allData = [];
     let offset = 0;
-
-    // Dynamically import fetch
-    const { default: fetch } = await import('node-fetch');
 
     do {
         params.offset = offset;
@@ -91,18 +89,7 @@ app.post('/scrap', async (req, res) => {
     }
 });
 
-app.listen(3000, '0.0.0.0', () => {
-    console.log("Server berjalan di http://0.0.0.0:3000");
+// Jalankan server
+app.listen(3000, () => {
+    console.log("Server berjalan di http://localhost:3000");
 });
-
-const path = require('path');
-
-// Menyajikan file statis seperti index.html
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Atau rute khusus untuk index.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-
